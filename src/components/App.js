5a56c90ee32602498907979047800e4a65d9a3e6
Dishank7 <127612933+Dishank7@users.noreply.github.com>
2024-01-22 04:59:27 +0000

    serach done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 
-import React from "react";
+import React , {useEffect, useState} from "react";
 import './../styles/App.css';
 
+const Fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+
+
 const App = () => {
+
+  const[search , setsearch] = useState('')
+  const[country , setcountry] = useState(Fruits)
+
+
+  useEffect(()=>{
+   setcountry(country.filter((value)=>{return(value.toLocaleLowerCase().includes(search.toLocaleLowerCase().trim()))}))
+  },[search])
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h3>Search Item</h3>
+        <input type="text" onChange={(e)=>setsearch(e.target.value)} />
+        <ul>
+          
+            {
+              country.map((value)=>{
+                return(
+               <li>{value}</li>
+                )
+              })
+            }
+          
+        </ul>
     </div>
   )
 }
